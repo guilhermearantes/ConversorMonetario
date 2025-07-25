38bf4ff2366d56081c3319b31edee8699a7db310
Guilherme Arantes <guilherme.jannotti@gmail.com>
2025-07-25 13:56:41 -0300

    chore: configuração inicial do projeto Spring Boot

---
+++ src/main/java/com/guilherme/desafiointer/remessa/DesafioJavaInterApplication.java
@@ -0,0 +1,13 @@
+
+
+package com.guilherme.desafiointer.remessa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJavaInterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioJavaInterApplication.class, args);
+    }
+}
